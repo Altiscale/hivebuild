596eb5c8f1212209660d68a5872307adf544420f
sookim-altiscale <sookim@altiscale.com>
2016-10-31 10:54:57 -0700

    HD-1745: Synchronize the code

--- scripts/justbuild.sh
+++ scripts/justbuild.sh
@@ -1,9 +1,39 @@
 #!/bin/sh -ex
 # build the artifacts using the hive ant build
+# set the hadoop version for the lowest common denominator and let it be overridden by env
+# sepcify $ADDITIONAL_OPTIONS for any fixed options "-Dskip.javadoc=true", for instance
+#export HADOOP_VERSION=${HADOOP_VERSION:-2.0.5-alpha}
+#export HIVE_ORC_PROTO_FILE=${HIVE_ORC_PROTO_FILE:-${WORKSPACE}/hive/ql/src/protobuf/org/apache/hadoop/hive/ql/io/orc/orc_proto.proto}
+#export HIVE_ORC_SRC_DIR=${HIVE_ORC_SRC_DIR:-${WORKSPACE}/hive/ql/src/protobuf/org/apache/hadoop/hive/ql/io/orc}
+#export HIVE_ORC_DST_DIR=${HIVE_ORC_DST_DIR:-${WORKSPACE}/hive/ql/src/gen/protobuf/gen-java}
+#if [[ -z "$HADOOP_PROTOC_PATH" ]]
+#then
+#  echo "Skipping protobuf compilation as HADOOP_PROTOC_PATH is null."
+#else
+#	$HADOOP_PROTOC_PATH -I=$HIVE_ORC_SRC_DIR --java_out=$HIVE_ORC_DST_DIR $HIVE_ORC_PROTO_FILE
+#fi
 
 #refixing wb-803 for wb-505, this would need to be a community patch, and this is  already changed in hive-0.11
-cp ${WORKSPACE}/scripts/data.hive-exec-log4j.properties ${WORKSPACE}/hive/data/conf/hive-log4j.properties
-cp ${WORKSPACE}/scripts/ql.hive-exec-log4j.properties ${WORKSPACE}/hive/ql/src/java/conf/hive-exec-log4j.properties
-cp ${WORKSPACE}/scripts/pdk.log4j.properties ${WORKSPACE}/hive/pdk/scripts/conf/log4j.properties
-cp ${WORKSPACE}/scripts/hive-log4j.properties ${WORKSPACE}/hive/common/src/java/conf/hive-log4j.properties
-ant clean package tar -Dhadoop.version=2.0.2-alpha -Dhadoop-0.23.version=2.0.2-alpha -Dhadoop.mr.rev=23 -Dversion=${ARTIFACT_VERSION}
+#export ANT_OPTS="-XX:MaxPermSize=512m -Xmx1024m"
+#mvn install -Pdist -DskipTests -Phadoop-2 -DcreateChecksum=true
+
+# See https://issues.apache.org/jira/browse/CALCITE-756 . We are installing untrusted code just to build hive for now.
+# WE SHOULD REMOVE THIS ASAP.
+wget http://conjars.org/repo/org/pentaho/pentaho-aggdesigner-algorithm/5.1.5-jhyde/pentaho-aggdesigner-algorithm-5.1.5-jhyde.pom
+wget http://conjars.org/repo/org/pentaho/pentaho-aggdesigner-algorithm/5.1.5-jhyde/pentaho-aggdesigner-algorithm-5.1.5-jhyde.jar
+wget http://conjars.org/repo/org/pentaho/pentaho-aggdesigner/5.1.5-jhyde/pentaho-aggdesigner-5.1.5-jhyde.pom
+
+# Install these untrusted artifacts in our local maven cache so that we can build hive.
+mvn install:install-file -Dfile=pentaho-aggdesigner-algorithm-5.1.5-jhyde.jar -DgroupId=org.pentaho -DartifactId=pentaho-aggdesigner-algorithm -Dpackaging=jar -Dversion=5.1.5-jhyde
+mvn install:install-file -Dfile=pentaho-aggdesigner-algorithm-5.1.5-jhyde.pom -DgroupId=org.pentaho -DartifactId=pentaho-aggdesigner-algorithm -Dpackaging=pom -Dversion=5.1.5-jhyde
+mvn install:install-file -Dfile=pentaho-aggdesigner-5.1.5-jhyde.pom -DgroupId=org.pentaho -DartifactId=pentaho-aggdesigner -Dpackaging=pom -Dversion=5.1.5-jhyde
+
+# Build hive
+mvn install -Pdist -DskipTests -Phadoop-2 -DcreateChecksum=true -Dmaven.javadoc.skip=true -Dmaven-javadoc-plugin=false -Dtez.version=${TEZ_VERSION}
+
+# Discover the path of the local maven cache
+MAVEN_LOCAL_REPO=`mvn help:evaluate -Dexpression=settings.localRepository | egrep -v '[INFO]|Download'`
+
+# Remove the untrusted artifacts from this node's local maven repo soon as we have built hive
+find $MAVEN_LOCAL_REPO -name 'pentaho-aggdesigner*' -type d | xargs rm -rf
+
--- scripts/justinstall.sh
+++ scripts/justinstall.sh
@@ -1,21 +1,26 @@
 #!/bin/sh -ex
-# deal with the hive artifacts to create a tarball
+# deal with the hive artifacts to create a tarball ARTIFACT_VERSION is supplied by the ruby wrapper
 ALTISCALE_RELEASE=${ALTISCALE_RELEASE:-0.1.0}
+HIVE_VERSION=${ARTIFACT_VERSION:-0.11.0}
+RPM_DESCRIPTION="Apache Hive ${HIVE_VERSION}\n\n${DESCRIPTION}"
 
-# convert each tarball into an RPM
+#convert each tarball into an RPM
 DEST_ROOT=${INSTALL_DIR}/opt
 mkdir --mode=0755 -p ${DEST_ROOT}
 cd ${DEST_ROOT}
-tar -xvzpf ${WORKSPACE}/hive/build/hive-${ARTIFACT_VERSION}.tar.gz
+tar -xvzpf ${WORKSPACE}/hive/packaging/target/apache-hive-${HIVE_VERSION}-bin.tar.gz
+tar -xvzpf ${WORKSPACE}/hive/packaging/target/apache-hive-${HIVE_VERSION}-src.tar.gz
+mv apache-hive-${HIVE_VERSION}-bin hive-${HIVE_VERSION}
+mv apache-hive-${HIVE_VERSION}-src hive-${HIVE_VERSION}/src
+
 mkdir --mode=0755 -p ${INSTALL_DIR}/etc
 mv ${INSTALL_DIR}/opt/hive-${ARTIFACT_VERSION}/conf ${INSTALL_DIR}/etc/hive-${ARTIFACT_VERSION}
 cd ${INSTALL_DIR}/opt/hive-${ARTIFACT_VERSION}
 ln -s /etc/hive-${ARTIFACT_VERSION} conf
+#fix a missed permission setting on hcat
+chmod 755 hcatalog/bin/hcat
 cd ${INSTALL_DIR}/opt/hive-${ARTIFACT_VERSION}/lib
 ln -s /opt/mysql-connector/mysql-connector.jar mysql-connector.jar
-
-# wb-803 remove slf4j-log4j12-1.6.1.jar
-rm ${INSTALL_DIR}/opt/hive*/lib/slf4j-log4j12-1.6.1.jar
 
 # convert all the etc files to config files
 cd ${INSTALL_DIR}
@@ -28,18 +33,19 @@ rm -f /tmp/$$.files
 
 cd ${RPM_DIR}
 
-export RPM_NAME=vcc-hive-${ARTIFACT_VERSION}
+export RPM_NAME="alti-hive-${HIVE_VERSION}"
 fpm --verbose \
---maintainer ops@verticloud.com \
---vendor VertiCloud \
+--maintainer support@altiscale.com \
+--vendor Altiscale \
 --provides ${RPM_NAME} \
---description "${DESCRIPTION}" \
+--description "${RPM_DESCRIPTION}" \
 --depends alti-mysql-connector \
---replaces vcc-hive \
+--url ${GITREPO} \
+--license "Apache License v2" \
 -s dir \
 -t rpm \
 -n ${RPM_NAME} \
--v ${RPM_VERSION} \
+-v ${ALTISCALE_RELEASE} \
 --iteration ${DATE_STRING} \
 ${CONFIG_FILES} \
 --rpm-user root \
