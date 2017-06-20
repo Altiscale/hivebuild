c8949538bee26144512a6abc6b9a1edc7b0eb594
Johannes Alberti <johannes@altiscale.com>
2017-06-20 16:21:45 -0700

    Introduce IGNORE_TESTFAILURES_BOOL to allow running of tests and ignoring of errors

--- scripts/justbuild.sh
+++ scripts/justbuild.sh
@@ -4,5 +4,13 @@ MVN_SKIPTESTS_BOOL=${SKIPTESTS_BOOL:-false}
 if [ "${MVN_SKIPTESTS_BOOL}" != "true" ] ; then
     MVN_SKIPTESTS_BOOL=false
 fi
+
+MVN_IGNORE_TESTFAILURES_BOOL=${IGNORE_TESTFAILURES_BOOL:-false}
+if [ "${MVN_IGNORE_TESTFAILURES_BOOL}" != "true" ] ; then
+    MVN_IGNORE_TESTFAILURES_BOOL=false
+fi
 # Build hive
-mvn install -Pdist -Dmaven.test.skip=${MVN_SKIPTESTS_BOOL} -DskipTests=${MVN_SKIPTESTS_BOOL} -DcreateChecksum=true -Dmaven.javadoc.skip=true -Dmaven-javadoc-plugin=false
+mvn install -Pdist \
+-Dmaven.test.skip=${MVN_SKIPTESTS_BOOL} -DskipTests=${MVN_SKIPTESTS_BOOL} \
+-Dmaven.test.failure.ignore=${MVN_IGNORE_TESTFAILURES_BOOL} -DtestFailureIgnore=${MVN_IGNORE_TESTFAILURES_BOOL} \
+-DcreateChecksum=true -Dmaven.javadoc.skip=true -Dmaven-javadoc-plugin=false
